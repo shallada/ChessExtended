16418b997ef41399c0d394a00de23e6db95caf77
Joseph Bass <jab416171@gmail.com>
2011-09-07 15:53:41 -0600

    implemented MoveHistory

--- src/edu/neumont/learningChess/api/MoveHistory.java
+++ src/edu/neumont/learningChess/api/MoveHistory.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 // TODO this class is a list of move histories from a completed game
 public class MoveHistory {
-
+	private final List<ExtendedMove> moves;
+	public MoveHistory(List<ExtendedMove> moves) {
+		this.moves = moves;
+		
+	}
 	public List<ExtendedMove> getMoves() {
-		return null;
+		return moves;
 	}
 
 }
