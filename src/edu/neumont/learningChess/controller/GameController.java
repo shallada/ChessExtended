44b6ed8cbf96d4cb15f7bb46713f5177e3e99d43
neuPanda <spainman0@yahoo.com>
2011-09-09 15:39:56 -0600

    piece update

--- src/edu/neumont/learningChess/controller/GameController.java
+++ src/edu/neumont/learningChess/controller/GameController.java
@@ -59,7 +59,7 @@ public class GameController implements IListener, ICheckChecker {
 
 	private boolean showDisplay;
 
-	private static final boolean ALWAYS_SHOW_BOARD = false;
+	private static final boolean ALWAYS_SHOW_BOARD = false;//false for check in
 
 	private List<ChessGameState> history = new ArrayList<ChessGameState>();
 
