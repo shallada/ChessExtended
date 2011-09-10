86fd43e8ab2b1323d9ca7120a60d833ae27d597b
Joseph Bass <jab416171@gmail.com>
2011-09-09 19:38:21 -0600

    BoardDisplay: Fixed doNothingOnClose

--- src/edu/neumont/learningChess/view/BoardDisplay.java
+++ src/edu/neumont/learningChess/view/BoardDisplay.java
@@ -40,11 +40,7 @@ public class BoardDisplay extends JFrame implements KeyListener, MouseListener, 
 		layeredPane.addMouseListener(this);
 		layeredPane.addMouseMotionListener(this);
 		this.addKeyListener(this);
-		// The client automatically sends the server the game history when the game is over,
-		// and then the window closes itself. This is so the user doesn't inadvertently close
-		// the window before the data is sent and a response is sent back.
-		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-
+		
 		chessBoard = new JPanel();
 		layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
 		chessBoard.setLayout(new GridLayout(N_ROWS, N_COLS));
@@ -65,9 +61,11 @@ public class BoardDisplay extends JFrame implements KeyListener, MouseListener, 
 				squareColor = getOtherColor(squareColor);
 			}
 		}
-		
-		
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+			
+		// The client automatically sends the server the game history when the game is over,
+		// and then the window closes itself. This is so the user doesn't inadvertently close
+		// the window before the data is sent and a response is sent back.
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		pack();
 		setResizable(true);
 		setLocationRelativeTo(null);
