9b2da73c1e79575f2aad475e2aefa12e64a9e999
Joseph Bass <jab416171@gmail.com>
2011-09-15 10:39:01 -0600

    Mode it so a new Gson object is made every time, to hopefully prevent a threading issue

--- src/edu/neumont/learningChess/json/Jsonizer.java
+++ src/edu/neumont/learningChess/json/Jsonizer.java
@@ -4,13 +4,11 @@ import com.google.gson.Gson;
 
 public class Jsonizer {
 	
-	static Gson gson = new Gson();
-	
 	public static <T> T dejsonize(String jsonString, Class<T> cls) {
-		return gson.fromJson(jsonString, cls);
+		return new Gson().fromJson(jsonString, cls);
 	}
 	
 	public static <T> String jsonize(T object) {
-		return gson.toJson(object);
+		return new Gson().toJson(object);
 	} 
 }
