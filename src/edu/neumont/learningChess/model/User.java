df01da2600176e431060d4dd9cc63277fa55b5c7
Joseph Bass <jab416171@gmail.com>
2011-09-19 14:10:00 -0600

    removed email field

--- src/edu/neumont/learningChess/model/User.java
+++ src/edu/neumont/learningChess/model/User.java
@@ -2,7 +2,6 @@ package edu.neumont.learningChess.model;
 
 public class User {
 	private String username;
-	private String email;
 	private String password;
 	
 	public User() {
@@ -21,21 +20,6 @@ public class User {
 	 */
 	public void setUsername(String username) {
 		this.username = username;
-	}
-
-	/**
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
-	}
-
-	/**
-	 * @param email
-	 *            the email to set
-	 */
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 	/**
