3a4573b620f21dfec1cf14d0af681609e8eccb7b
Joseph Bass <jab416171@gmail.com>
2011-09-19 16:26:05 -0600

    removed unnecessary line

--- src/Main.java
+++ src/Main.java
@@ -31,8 +31,6 @@ import edu.neumont.learningChess.model.TextCommandProcessorOutput;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(MD5("1234"));
-		System.exit(0);
 		do {
 			ThemeNames[] values = ThemeNames.values();
 			String[] themeNames = new String[values.length];
