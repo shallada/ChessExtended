3edccb51d2032cf5f4fc5822a4bba8c498dedbb1
Joseph Bass <jab416171@gmail.com>
2011-09-19 14:00:04 -0600

    added user class

---
+++ src/edu/neumont/learningChess/model/User.java
@@ -0,0 +1,55 @@
+package edu.neumont.learningChess.model;
+
+public class User {
+	private String username;
+	private String email;
+	private String password;
+	
+	public User() {
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username
+	 *            the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
